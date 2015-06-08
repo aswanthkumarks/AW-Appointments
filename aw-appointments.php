c92919ffc91547bd8cf8b1540ab85477e287a264
Aswanth <aswanthkumar25@gmail.com>
2015-06-08 18:21:29 +0530

    Create aw-appointments.php

---
+++ aw-appointments.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Plugin Name: AW Appointments
+ * Author: Aswanth kumar
+ * Text Domain: aw-appointments
+ * Description: Plugin for taking appointments from visitors
+ * Version: 0.1
+ */
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+define( 'AW_APPOINTMENT_VERSION', '0.1' );
+define( 'AW_APPOINTMENT_MINIMUM_WP_VERSION', '3.1' );
+define( 'AW_APPOINTMENT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'AW_APPOINTMENT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'AW_APPOINTMENT_DELETE_LIMIT', 100000 );
+
+register_activation_hook( __FILE__, array( 'Appointments', 'plugin_activation' ) );
+register_deactivation_hook( __FILE__, array( 'Appointments', 'plugin_deactivation' ) );
+
+
+require_once( AW_APPOINTMENT_PLUGIN_DIR . 'class.appointments.php' );
+add_action( 'init', array( 'appointments', 'init' ) );
+
+if ( is_admin() ) {
+	require_once( AW_APPOINTMENT_PLUGIN_DIR . 'class.appointments-admin.php' );
+	add_action( 'init', array( 'appointmentadmin', 'init' ) );
+}
+?>
