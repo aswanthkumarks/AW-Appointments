652e15054d8ff581c427637e54f289e1983804ba
Aswanth <aswanthkumar25@gmail.com>
2015-06-08 18:22:35 +0530

    Create class.appointments-admin.php

---
+++ class.appointments-admin.php
@@ -0,0 +1,29 @@
+<?php
+class Appointmentadmin{
+	
+	private static $initiated = false;
+	
+	public static function init() {
+		if ( ! self::$initiated ) {
+			self::init_hooks();
+		}
+	
+	}
+	
+	public static function init_hooks() {
+		self::$initiated = true;
+		add_action( 'admin_init', array( 'appointmentadmin', 'admin_init' ) );
+		add_action( 'admin_menu', array( 'appointmentadmin', 'admin_menu' ) );
+	}
+	
+	public static function admin_init() {
+		load_plugin_textdomain( 'aw-appointments' );
+	}
+	
+	public static function admin_menu() {
+		add_menu_page('Appointments', 'aw-appointments', '','aw-appointments/admin.php','','dashicons-calendar-alt', 4.55);
+	}
+	
+	
+}
+?>
