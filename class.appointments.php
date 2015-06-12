73b632275654863f20d5aacf568e8ebe1c0a0509
aswanthkumarks <aswanthkumar25@gmail.com>
2015-06-12 18:27:53 +0530

    Appointment form created
    javascript validation done

--- class.appointments.php
+++ class.appointments.php
@@ -22,22 +22,240 @@ class Appointments{
 			],
 	];
 	}
+	private static $dayname=[
+			'mon'=>'Monday',
+			'tue'=>'Tuesday',
+			'wed'=>'Wednesday',
+			'thu'=>'Thursday',
+			'fri'=>'Friday',
+			'sat'=>'Saturday',
+			'sun'=>'Sunday',
+	];
+	private static $week=[
+			'mon'=>1,
+			'tue'=>2,
+			'wed'=>3,
+			'thu'=>4,
+			'fri'=>5,
+			'sat'=>6,
+			'sun'=>0,
+	];
 	
 	private static $noapp = ['f'=>'','t'=>''];
+	private static $initiated = false;
+	private static $settings = [];
 	
 	public static function init() {
-		add_shortcode('aw-appointments', array('appointments', 'shortcode'));		
+		if ( ! self::$initiated ) {
+			add_shortcode('aw-appointment', array('appointments', 'shortcode'));
+			self::init_hooks();
+			self::$settings=json_decode(get_option('aw-appointments'),true);
+		}	
+	}
+	
+	public static function init_hooks() {
+		self::$initiated = true;
+		add_action( 'wp_enqueue_scripts', array( 'appointments', 'load_resources' ) );
+		add_action('wp_ajax_aw_appointment', array('appointments','aw_appointment'));
+	}
+	
+	public static function load_resources(){
+		wp_register_style( 'jquery-ui', '//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css', array(), 1.11 );
+		wp_enqueue_style( 'jquery-ui');		
+		wp_register_script( 'jquery-ui', '//code.jquery.com/ui/1.11.4/jquery-ui.js', array('jquery'), 1.11 );
+		wp_enqueue_script( 'jquery-ui' );
+	}
+	public function getSettings(){
+		return self::$settings;
 	}
 	
 	private function appointmenttable(){
 		global $wpdb;
 		return $wpdb->prefix . 'aw_appointments';
-	}	
+	}
 	
-	public function shortcode(){
+	private static function between_dates($start,$end){
+		$ret=[];
+		$datediff = strtotime($end) - strtotime($start);
+		$datediff = floor($datediff/(60*60*24));
+		for($i = 0; $i < $datediff + 1; $i++){
+			array_push($ret,date("n-j-Y", strtotime($start . ' + ' . $i . 'day')));
+		}
+		return $ret;
+	}
+	
+	public static function shortcode($atts){
+		$opt=self::$settings;
+		$disabled=[];
+		$disweek=[];
+		$min=date("n-j-Y",strtotime(date("Y-m-d", strtotime('now')) . " +".$opt['pm']."days"));
+		$max=date("n-j-Y",strtotime(date("Y-m-d", strtotime('now')) . " +".$opt['px']."days"));
 		
+		foreach ($opt['timing'] as $key=>$val){
+			if(!$val['s']) {
+					array_push($disweek, self::$week[$key]);
+			}
+		}
 		
+		foreach($opt['noapp'] as $ap){
+			$disabled=array_merge($disabled,self::between_dates($ap['f'], $ap['t']));
+		}
 		
+	
+		switch($atts['theam']){
+			case 'basic': require_once "template/basic.php"; break;
+			default: require_once "theam/basic.php"; break;
+		}
+		
+		?>
+		<script type="text/javascript">
+		(function($){
+
+
+			var disableddates = ["12-3-2014", "12-11-2014", "12-25-2014", "12-20-2014"];
+			function DisableSpecificDates(date) {
+				var disweek = <?php echo json_encode($disweek); ?>;
+				var disabledate =<?php echo json_encode($disabled); ?>;
+				var off=false;
+
+				var m = date.getMonth();
+				var d = date.getDate();
+				var y = date.getFullYear();
+
+				
+				var dnow=(m+1)+'-'+d+'-'+y;
+				if ($.inArray(dnow, disabledate) !== -1 ) {
+					off=true;
+				 	return [false];
+				}
+				
+				if(!off){	 
+				 var day = date.getDay();
+				 if ($.inArray(day,disweek) !== -1) {				 
+				 	return [false] ;				 
+				 }
+				 else {					 			 				 
+				  return [true] ;
+				 }
+				}
+			 
+			}
+			
+    		$( ".aw-date" ).datepicker({
+    			 beforeShowDay: DisableSpecificDates,
+    			 minDate: new Date('<?php echo $min; ?>'),
+    			 maxDate: new Date('<?php echo $max; ?>'),
+    			 onSelect: function(d,i){
+    		          if(d !== i.lastVal){
+    		              $(this).change();
+    		          }
+    		     },
+    		 });
+
+      		
+
+      		 $(".aw-input").change(function(){
+      			var inputval=$(this).val();
+      			var valid=validateform($(this),inputval);       		
+          		if(valid){
+              		if($(this).hasClass('aw-date')){
+              			var d = new Date(inputval);
+              			var n = d.getDay();
+              			$('.aw-week').addClass('aw-hideday');
+              			$('#aw-slot-'+n).removeClass('aw-hideday');
+              			$('.aw-week input').removeAttr('checked');
+                  	}              		
+          			$(this).removeClass("aw-error");
+              		}
+          		else{
+          			$(this).addClass("aw-error");
+              		}
+          	});
+           	$('.aw-slot input').click(function(){
+               	$(this).closest('.aw-weeks').removeClass("aw-error");
+
+            });
+
+            $('.aw-btn-active').click(function(){
+                var formvalid=true;
+            	var re = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+            	var obj=$(this).closest('.aw-appointment');
+            	var data=obj.find('form').serializeArray();
+            	
+            	$.each(data,function(k,v){
+                	var valid=validateform(obj.find('[name='+v.name+']'),v.value);               	         		
+              		if(valid){
+              			obj.find('[name='+v.name+']').removeClass("aw-error");
+                  		}
+              		else{
+              			obj.find('[name='+v.name+']').addClass("aw-error");
+              			formvalid=false;
+                  		}
+                	});
+            	
+            	if(typeof obj.find('[type="radio"]:checked').val() == 'undefined'){
+            		obj.find('.aw-weeks').addClass('aw-error');
+            		formvalid=false;
+                }
+            	
+            	if(formvalid){
+                $(this).removeClass('aw-btn-active');
+                $(this).addClass('aw-btn-deactive');
+            	}
+            });
+
+            
+
+            function validateform(obj,inputval){
+            	if(obj.hasClass('aw-date')){              		
+          			var re = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+          			if(!inputval.match(re)){
+          				valid=false;             	
+                    }
+            		else{
+            			valid=true;            			
+                    }
+              	}
+              		else if(obj.hasClass('aw-name')){
+                  		if(inputval.length<4){ valid=false; }
+                  		else{ valid=true; }
+                  	}
+              		else if(obj.hasClass('aw-email')){
+              			var eregx = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                    	if(!eregx.test(inputval)){ valid=false; }
+                  		else{ valid=true; }
+                  	}
+              		else if(obj.hasClass('aw-phone')){
+                  		var preg=/^\+?([0-9]{2,3})\)?[-. ]?([0-9]{3,4})[-. ]?([0-9]{4,7})$/;
+                  		var preg1=/^\(?([0-9]{3,4})\)?[-. ]?([0-9]{4,7})$/;
+                  		var preg2=/^\(?([0-9]{2,3})\)?[-. ]?([0-9]{3,4})[-. ]?([0-9]{4,7})$/;
+                  		var preg3=/^\d{9,10}$/;
+                  		if(preg.test(inputval)||preg1.test(inputval)||preg2.test(inputval)||preg3.test(inputval)){
+                      		valid=true;
+                      	}
+                  		else{ valid=false; }
+                  	}
+              		else if(obj.hasClass('aw-city')){
+              			if(inputval.length<3) valid=false;
+              			else valid=true;
+                  	}
+              		else if(obj.hasClass('aw-country')){
+              			if(inputval.length<3) valid=false;
+              			else valid=true;
+                  	}
+              		else if(obj.hasClass('aw-address')){
+              			if(inputval.length<5||inputval.length>200) valid=false;
+              			else valid=true;
+                  	}
+
+              	return valid;
+
+            }
+     		 
+		})(jQuery);
+  		</script>
+		
+		<?php 
 	}	
 	
 	/**
---
+++ template/basic.php
@@ -0,0 +1,131 @@
+<div class="aw-appointment basic">
+<h2 class="aw-head">Take Appointment</h2>
+<form>
+<input type="text" class="aw-date aw-input" placeholder="Choose Date" name="aw-date"/>
+
+<div class="aw-weeks">
+<?php foreach($opt['timing'] as $key=>$value){
+	if($value['s']){
+		echo '<div id="aw-slot-'.appointments::$week[$key].'" class="aw-week aw-hideday"><div class="aw-slotkey">'.appointments::$dayname[$key].' :</div>';
+		foreach($value['t'] as $skey=>$slot){
+			echo '<div class="aw-slot"><input name="aw-slot" data-slot="'.$slot['n'].'" id="aw-'.$key.'-'.$skey.'" type="radio" value="'.$skey.'"/><label for="aw-'.$key.'-'.$skey.'">'.$slot["f"].'-'.$slot["t"].'</label></div>';
+		}		
+		echo '</div>';
+	}	
+	
+} ?>
+
+</div>
+<input type="text" class="aw-name aw-input" placeholder="Your name" name="aw-name"/>
+<input type="email" class="aw-email aw-input" placeholder="Your email" name="aw-email"/>
+<input type="tel" class="aw-phone aw-input" placeholder="Your phone" name="aw-phone"/>
+<hr/>
+<div class="aw-locations"><label>Your Location Details</label>
+<textarea class="aw-address aw-input" name="aw-address" placeholder="Your address"></textarea>
+<input type="text" class="aw-city aw-input" placeholder="City" name="aw-city"/>
+<input type="text" class="aw-country aw-input" placeholder="Country" name="aw-country"/>
+</div>
+<div class="aw-submit"><button type="button" class="aw-button aw-btn-active">Submit</button></div>
+</form>
+</div>
+
+
+<style type="text/css">
+.aw-appointment{
+	border:1px solid #ccc;
+}
+.aw-appointment hr{
+padding:0; 
+margin:5px 0;
+}
+.aw-appointment .aw-submit{
+text-align:center;
+}
+.aw-appointment button.aw-button{
+	background-color: #0392ff;
+    border: 0;
+    outline:0;
+    padding:0;
+    margin:0 auto;
+    color: #FFF;
+    cursor: pointer;
+    height: 35px;
+    width: 100px;
+    font-size:13px;
+    line-height:35px;
+    box-shadow:0 0 2px #333;
+}
+.aw-appointment button.aw-button:hover{
+	background-color:#299FF9;
+}
+.aw-appointment button.aw-button:active{
+	position:relative;
+	top:-1px;
+}
+.aw-appointment .aw-btn-deactive{
+	background: #0392ff url('<?php echo AW_APPOINTMENT_PLUGIN_URL; ?>loadingbtn.gif') no-repeat;
+	  padding-left: 29px !important;
+  background-position-y: center;
+  background-size: 25px 25px;
+  background-position-x: 4px;
+}
+
+.aw-appointment input.aw-input{
+	margin:2px 0;
+	width:100%;
+	padding: 0.5278em;
+	background:#F7F7F7;
+	color:#707070;
+	color:rgba(51, 51, 51, 0.7);
+}
+.aw-appointment .aw-locations{
+	margin: 0.5em 1em;
+}
+.aw-appointment input.aw-input:focus{
+  outline:0;
+  background-color: #fff;
+  border: 1px solid #c1c1c1;
+  border: 1px solid rgba(51, 51, 51, 0.3);
+  color: #333;
+
+}
+
+.aw-appointment.basic{
+	padding:3px;
+}
+.aw-appointment .aw-hideday{
+display:none;
+}
+.aw-appointment .aw-week{
+	border:1px solid #ccc;
+	margin:3px 0;	
+	font-size:12px;
+}
+.aw-appointment .aw-slot{
+	display:inline-block;
+	font-size:12px;
+	margin:2px 5px;		
+	padding:3px;
+}
+.aw-appointment .aw-slotkey{
+	display:inline-block;
+	padding:3px;
+}
+
+.aw-appointment .aw-slot label{
+	display:inline-block;
+	cursor:pointer;
+}
+.aw-appointment .aw-slot input{
+	margin:0 4px;
+	cursor:pointer;
+}
+.aw-appointment h2.aw-head{
+	font-size:16px;
+	padding:3px;
+	margin:4px 0;
+}
+.aw-appointment .aw-error{
+	border:1px solid #ea1010;
+}
+</style>
